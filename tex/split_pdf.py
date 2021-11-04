c144ca1100573b8b8b723f8c6f73187c66a8db54
Vishnu Nittoor <nitvishn@gmail.com>
2021-11-04 14:54:26 -0400

    updated PDF splitter to have funny names

--- tex/split_pdf.py
+++ tex/split_pdf.py
@@ -2,25 +2,27 @@ from os import lseek
 import PyPDF2
 import re
 
-filename = 'mat157_hw5.pdf'
+filename = 'mat157_hw6.pdf'
 writer = PyPDF2.PdfFileWriter()
 currentlyWritingTo = None
 with open(filename, 'rb') as f:
     reader=PyPDF2.PdfFileReader(f)
     for i in range(reader.getNumPages()):
         page = reader.getPage(i)
-        text = page.extractText().split('\n')[0:5]
-        if re.search(r'^[0-9]+[a-zA-Z0-9:),\']*$', text[0]) :
-            print(text[0])
+        text = page.extractText().split('\n')[0:10]
+        # print(text)
+        if re.search(r'^[0-9]+[a-zA-Z0-9:),\']*$', text[0]):
+            questionFound = int(re.search(r'\d+', text[0]).group())
+            print(f"Found question {questionFound} on page {i+1}. We are currently writing to {currentlyWritingTo}.")
             if currentlyWritingTo:
-                print('ok', currentlyWritingTo)
-                
-                writer.write(open(f"submission/{currentlyWritingTo}.pdf", 'wb'))
-                writer = PyPDF2.PdfFileWriter()
-                currentlyWritingTo = text[0][0]
+                if questionFound == currentlyWritingTo + 1:
+                    print(f"Wrapping up question {currentlyWritingTo}.")
+                    writer.write(open(f"submission/{currentlyWritingTo}.pdf", 'wb'))
+                    writer = PyPDF2.PdfFileWriter()
+                    currentlyWritingTo = questionFound
             else:
-                print('lawl')
-                currentlyWritingTo = text[0][0]
+                currentlyWritingTo = questionFound
         writer.addPage(page)
+print(f"Finishing up by writing to {currentlyWritingTo}")
 writer.write(open(f"submission/{currentlyWritingTo}.pdf", 'wb'))
 writer = PyPDF2.PdfFileWriter()
